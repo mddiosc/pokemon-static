656424da953a3b5d4c36a7b093e4cf47787498ca
Miguel Ángel de Dios <mddiosc@gmail.com>
2022-03-18 20:16:20 +0100

    add Graph Meta tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -5,17 +5,21 @@ interface LayoutProps {
   title?: string;
 }
 
+const origin = typeof window !== "undefined" ? window.location : "";
+
 export const Layout: React.FC<LayoutProps> = ({ title, children }) => {
   return (
     <>
       <Head>
         <title>{title || "Pokemon App"} </title>
-        <meta name="author" content="Miguel Ángel de Dios" />
-        <meta
-          name="description"
-          content={`Información sobre el pokémon ${title}`}
-        />
         <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+
+        <meta property="og:title" content={`Información sobre ${title}`} />
+        <meta
+          property="og:description"
+          content={`Esta es la página sobre ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/images/banner.png`} />
       </Head>
 
       <Navbar></Navbar>
